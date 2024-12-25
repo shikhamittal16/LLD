4ed37b5a900e536da08ae491e0ec5448fd52ef45
ShikhaMittal231 <shikha.mittal@dogmasystems.com>
2024-12-25 17:15:15 +0530

    ATM LLD

--- src/ATM/TransactionImpl.java
+++ src/ATM/TransactionImpl.java
@@ -1,16 +1,21 @@
 package ATM;
 
-public class TransactionImpl {
-    Boolean withdrawMoney(double price , Account accountDetails) {
+public class TransactionImpl implements SubStates{
+
+    @Override
+    public Boolean withdrawMoney(double price, Account accountDetails) {
         return true;
     }
-    Boolean depositMoney(double price , Account accountDetails, String depositMethod){
+    @Override
+    public Boolean depositMoney(double price , Account accountDetails, String depositMethod){
         return true;
     }
-    Boolean transferMoney(double price , Account transferFrom , Account transferTo){
+    @Override
+    public Boolean transferMoney(double price , Account transferFrom , Account transferTo){
         return true;
     }
-    void balanceEnquiry(Account accountDetails) {
+    @Override
+    public void balanceEnquiry(Account accountDetails) {
         System.out.println(" ** your account balance is 50k");
     }
 }
